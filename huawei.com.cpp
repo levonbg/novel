6963941d5ac071884eee31862b7ba0976e0df71b
levonbg <32862171+levonbg@users.noreply.github.com>
2021-07-25 01:22:51 +0800

    Update huawei.com.cpp
    ggg

--- huawei.com.cpp
+++ huawei.com.cpp
@@ -1,3 +1,121 @@
+getProductDetails
+
+rcjc
+
+if (params.GetString("applicationID").empty()) {
+	break;
+}
+if (params.GetString("requestId").empty()) {
+	break;
+}
+if (params.GetString("merchantId").empty()) {
+	break;
+}
+if (params.GetString("productNos").empty()) {
+	break;
+}
+
+
+aaa.appId = params.GetString("applicationID");
+aaa.requestId = params.GetString("requestId");
+aaa.merchantId = params.GetString("merchantId");
+aaa.productNos = params.GetString("productNos");
+
+
+return
+succ
+JsonArray array;
+JsonObject obj;
+obj.Put("productNo", input.GetString("productNo"));
+obj.Put("microsPrice", input.GetLong("microsPrice"));
+obj.Put("price", input.GetString("price"));
+obj.Put("currency", input.GetString("currency"));
+obj.Put("country", input.GetString("country"));
+obj.Put("productName", input.GetString("productName"));
+obj.Put("productDesc", input.GetString("productDesc"));
+array.PushBack(obj);
+
+fail
+JsonArray array;
+obj.Put("productNo", input.GetString("productNo"));
+obj.Put("code", input.GetInt("code"));
+obj.Put("msg", input.GetString("msg"));
+array.PushBack(obj);
+
+
+
+
+
+getPurchaseInfo
+rcjc
+
+if (params.GetString("appId").empty()) {
+	break;
+}
+if (params.GetString("priceType").empty()) {
+	break;
+}
+if (params.GetString("merchantId").empty()) {
+	break;
+}
+if (params.GetLong("ts") == 0) {
+	break;
+}
+if (params.GetString("publicKey").empty()) {
+	break;
+}
+if (params.GetLong("pageNo") == 0) {
+	break;
+}
+if (params.GetString("sign").empty()) {
+	break;
+}
+aaa.appId = params.GetString("appId");
+aaa.priceType = params.GetString("priceType");
+aaa.merchantId = params.GetString("merchantId");
+aaa.ts = params.GetLong("ts");
+aaa.publicKey = params.GetString("publicKey");
+aaa.pageNo = params.GetLong("pageNo");
+aaa.sign = params.GetString("sign");
+aaa.productId = params.GetString("productId");
+
+return
+JsonArray array;
+obj.Put("requestId", input.GetString("requestId"));
+obj.Put("merchantId", input.GetString("merchantId"));
+obj.Put("appId", input.GetString("appId"));
+obj.Put("productId", input.GetString("productId"));
+obj.Put("tradeTime", input.GetString("tradeTime"));
+array.PushBack(obj);
+
+
+getOrderDetail
+if (params.GetString("requestId").empty()) {
+	break;
+}
+if (params.GetString("keyType").empty()) {
+	break;
+}
+if (params.GetString("merchantId").empty()) {
+	break;
+}
+if (params.GetString("time").empty()) {
+	break;
+}
+if (params.GetString("publicKey").empty()) {
+	break;
+}
+if (params.GetString("sign").empty()) {
+	break;
+}
+aaa.requestId = params.GetString("requestId");
+aaa.keyType = params.GetString("keyType");
+aaa.merchantId = params.GetString("merchantId");
+aaa.time = params.GetString("time");
+aaa.publicKey = params.GetString("publicKey");
+aaa.sign = params.GetLong("sign");
+
+
 
 pay.getPurchaseInfo(OBJECT)
 查询当前用户已订购的非消耗商品订单信息。
